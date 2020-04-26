1a0b8147f4074588dcb7957ac3dca39267910e4a
Paulo Candido <pcandido.m@gmail.com>
2020-04-25 23:06:18 -0300

    tipos complexos JPA

--- src/main/java/com/paulocandido/atendimento/model/Atendimento.java
+++ src/main/java/com/paulocandido/atendimento/model/Atendimento.java
@@ -3,9 +3,12 @@ package com.paulocandido.atendimento.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,8 +24,11 @@ public class Atendimento {
     private String sumario;
     private LocalDateTime criacao;
     private String descricao;
+    @Enumerated(EnumType.STRING)
     private TipoAtendimento tipo;
+    @ManyToOne
     private Cliente cliente;
+    @ManyToOne
     private Operador responsavel;
 
 }
