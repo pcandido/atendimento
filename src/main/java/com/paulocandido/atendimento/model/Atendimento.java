a3e851a9aa8d8a81b230b2a91580f8916c76d7da
Tutorial <pcandido.m@gmail.com>
2020-05-02 14:46:06 -0300

    documentação do modelo Atendimento

--- src/main/java/com/paulocandido/atendimento/model/Atendimento.java
+++ src/main/java/com/paulocandido/atendimento/model/Atendimento.java
@@ -13,21 +13,53 @@ import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Um atendimento é uma interação iniciada pelo cliente e atendida por um operador.
+ */
 @Data
 @NoArgsConstructor
 @Entity
 public class Atendimento {
 
+    /**
+     * Identificador do objeto
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * Breve descrição para que o operador possa iniciar o atendimento
+     * @see #descricao
+     */
     private String sumario;
+
+    /**
+     * Data e hora de criação do atendimento
+     */
     private LocalDateTime criacao;
+
+    /**
+     * Descrição detalhada sobre a requição
+     * @see #sumario
+     */
     private String descricao;
+
+    /**
+     * Tipo de atendimento de acordo com as opções disponíveis em {@link com.paulocandido.atendimento.model.TipoAtendimento Tipo de Atendimento}.
+     */
     @Enumerated(EnumType.STRING)
     private TipoAtendimento tipo;
+
+    /**
+     * Cliente que requisitou o atendimento
+     */
     @ManyToOne
     private Cliente cliente;
+
+    /**
+     * Operador responsável por dar prosseguimento ao atendimento
+     */
     @ManyToOne
     private Operador responsavel;
 
