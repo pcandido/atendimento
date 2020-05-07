120f46f4b37c27ef86b3d08f3f61ddb63c8c980c
Tutorial <pcandido.m@gmail.com>
2020-05-07 00:13:27 -0300

    ensaio de métodos rest parte 1

--- src/main/java/com/paulocandido/atendimento/controllers/HelloWorld.java
+++ src/main/java/com/paulocandido/atendimento/controllers/HelloWorld.java
@@ -1,14 +1,49 @@
 package com.paulocandido.atendimento.controllers;
 
+import java.util.List;
+
+import com.paulocandido.atendimento.model.Cliente;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @GetMapping(produces = "text/plain")
-    public String helloWorld(){
-        return "Hello World";
+    @GetMapping("clientes")
+    public List<Cliente> consultaCliente(@RequestParam String sort){
+            return List.of(
+                new Cliente(1L, "Paulo", "paulo@dominio.com"),
+                new Cliente(2L, sort, "ana@dominio.com")
+            );
+    }
+
+    @GetMapping("clientes/{id}")
+    public Cliente getById(@PathVariable Long id){
+        return new Cliente(id, "Paulo", "paulo@dominio.com");
+    }
+
+    @PostMapping("clientes")
+    public Cliente incluirCliente(@RequestBody Cliente body){
+        body.setId(5L);
+        return body;
+    }
+
+    @PutMapping("clientes/{id}")
+    public Cliente atualizarCliente(@RequestBody Cliente body, @PathVariable Long id){
+        body.setId(id);
+        return body;
+    }
+
+    @DeleteMapping("clientes/{id}")
+    public void removeCliente(@PathVariable Long id){
+        System.out.println(id);
     }
 
 }
---
+++ src/main/resources/materials/verbos_http.md
@@ -0,0 +1,7 @@
+# Verbos HTTP
+
+GET    {{host}}/clientes     -> Consultar a lista dos clientes
+GET    {{host}}/clientes/52  -> Consultar um cliente específico
+POST   {{host}}/clientes     -> Incluir um novo cliente
+PUT    {{host}}/clientes/52  -> Atualizar um cliente específico
+DELETE {{host}}/clientes/52  -> Remover um cliente específico
