f670cbda2fa1318d58894489c8fd1e1526f25dcb
Tutorial <pcandido.m@gmail.com>
2020-05-07 00:26:36 -0300

    ensaio de api rest parte 2

--- src/main/java/com/paulocandido/atendimento/controllers/HelloWorld.java
+++ src/main/java/com/paulocandido/atendimento/controllers/HelloWorld.java
@@ -1,6 +1,9 @@
 package com.paulocandido.atendimento.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.paulocandido.atendimento.model.Cliente;
 
@@ -10,40 +13,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
+    Long lastId = 0L;
+    Map<Long, Cliente> dados = new HashMap<>();
+
     @GetMapping("clientes")
-    public List<Cliente> consultaCliente(@RequestParam String sort){
-            return List.of(
-                new Cliente(1L, "Paulo", "paulo@dominio.com"),
-                new Cliente(2L, sort, "ana@dominio.com")
-            );
+    public List<Cliente> consultaCliente(){
+            return new ArrayList<>(dados.values());
     }
 
     @GetMapping("clientes/{id}")
     public Cliente getById(@PathVariable Long id){
-        return new Cliente(id, "Paulo", "paulo@dominio.com");
+        return dados.get(id);
     }
 
     @PostMapping("clientes")
     public Cliente incluirCliente(@RequestBody Cliente body){
-        body.setId(5L);
+        body.setId(++lastId);
+        dados.put(body.getId(), body);
         return body;
     }
 
     @PutMapping("clientes/{id}")
     public Cliente atualizarCliente(@RequestBody Cliente body, @PathVariable Long id){
-        body.setId(id);
+        dados.put(id, body);
         return body;
     }
 
     @DeleteMapping("clientes/{id}")
     public void removeCliente(@PathVariable Long id){
-        System.out.println(id);
+        dados.remove(id);
     }
 
 }
