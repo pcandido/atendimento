49fadcf6e6ba2c0c15a9d743f0c2e734f23aedff
Tutorial <pcandido.m@gmail.com>
2020-06-07 01:14:26 -0300

    Operador Repository

---
+++ src/main/java/com/paulocandido/atendimento/repository/OperadorRepository.java
@@ -0,0 +1,9 @@
+package com.paulocandido.atendimento.repository;
+
+import com.paulocandido.atendimento.model.Operador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperadorRepository extends JpaRepository<Operador, Long> {
+
+}
