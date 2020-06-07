449a27211926f49ddb11ec87de72a5a773228a11
Tutorial <pcandido.m@gmail.com>
2020-06-07 01:11:43 -0300

    Refatoração

--- src/main/java/com/paulocandido/atendimento/model/Cliente.java
+++ src/main/java/com/paulocandido/atendimento/model/Cliente.java
@@ -6,12 +6,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Cliente {
 
