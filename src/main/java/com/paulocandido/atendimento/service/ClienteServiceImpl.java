98df9834c743359e8da29f785db0e7895d4e6714
Tutorial <pcandido.m@gmail.com>
2020-07-11 21:57:01 -0300

    criação do clientService

--- src/main/java/com/paulocandido/atendimento/service/ClienteServiceImpl.java
+++ src/main/java/com/paulocandido/atendimento/service/ClienteServiceImpl.java
@@ -1,5 +1,7 @@
 package com.paulocandido.atendimento.service;
 
+import java.util.List;
+
 import com.paulocandido.atendimento.model.Cliente;
 import com.paulocandido.atendimento.repository.ClienteRepository;
 
@@ -16,11 +18,32 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public Cliente create(Cliente cliente) {
-        
-        if(repository.existsByEmail(cliente.getEmail()))
+
+        if (repository.existsByEmail(cliente.getEmail()))
             throw new RuntimeException();
 
         return repository.save(cliente);
+    }
+
+    @Override
+    public List<Cliente> list() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Cliente getById(long id) {
+        return repository.getOne(id);
+    }
+
+    @Override
+    public Cliente update(long id, Cliente cliente) {
+        cliente.setId(id);
+        return repository.save(cliente);
+    }
+
+    @Override
+    public void delete(long id) {
+        repository.deleteById(id);
     }
 
 }
